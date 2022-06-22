48415a97b97bbedd464bb45c54c4b14d958cb251
mbrit <mbrit.mail@gmail.com>
2022-06-22 11:13:41 +0200

    feat: adding icons

--- sopa_letras/views/include/list_view.php
+++ sopa_letras/views/include/list_view.php
@@ -3,7 +3,7 @@
     <thead>
         <tr>
             <th>Palabra</th>
-            <?php if ($_SESSION["perfil"] != "invitado" ) {?>
+            <?php if ($_SESSION["perfil"] != "invitado") { ?>
                 <th>Acciones</th>
             <?php } ?>
         </tr>
@@ -12,9 +12,11 @@
         <?php foreach ($data["infWord"] as $word) { ?>
             <tr>
                 <td> <?php echo $word["palabra"] ?> </td>
-                <?php if ($_SESSION["perfil"] != "invitado" ) {?>
-                    <td><button><a href="./edit/<?php echo $word["id"]?>">edit</a></button></td>
-                    <td><button><a href="./del/<?php echo $word["id"]?>">del</a></button></td>
+                <?php if ($_SESSION["perfil"] != "invitado") { ?>
+                    <td><button>
+                            <a href="./edit/<?php echo $word["id"] ?>"><span class="material-icons">edit</span></a>
+                        </button></td>
+                    <td><button><a href="./del/<?php echo $word["id"] ?>"><span class="material-icons">delete</span></a></button></td>
                 <?php } ?>
             </tr>
         <?php } ?>
